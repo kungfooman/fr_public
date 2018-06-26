425a31bb2f388ea100f589df10a4191eada7130b
kungfooman <lama12345@gmail.com>
2018-06-26 10:29:40 +0200

    wz dumpWindows with imgui_draw_wz_rect stuff in DockScript

--- altona_wz4/altona/main/base/imgui_wz4/dock_script.cpp
+++ altona_wz4/altona/main/base/imgui_wz4/dock_script.cpp
@@ -28,7 +28,7 @@
 #include "../imgui_docks/dock_console.h"
 
 
-
+#include "../imgui/imgui_api.h"
 extern sGui_ *sGui;
 
 
@@ -39,14 +39,89 @@ const char *DockScript::label() {
 	return "Script";
 }
 
+class TestMessage : public sObject { public:
+	void bla() {
+		imgui_log("blaaaa");
+	}
+};
+
+TestMessage *testMessage = NULL;
+
+void imgui_draw_wz_rect(sRect &rect, unsigned int color) {
+	float left = rect.x0;
+	float top = rect.y0;
+	float right = rect.x1;
+	float bottom = rect.y1;
+
+	ImVec2 a;
+	ImVec2 b;
+
+	// from top/left to bottom/right
+	a = ImVec2(left, top);
+	b = ImVec2(bottom, right);
+	//imgui_line(&a, &b, color, 1);
+	
+
+	a = ImVec2(left, top);
+	b = ImVec2(right, top);
+	imgui_line(&a, &b, color, 1);
+
+	
+	a = ImVec2(left, bottom);
+	b = ImVec2(right, bottom);
+	imgui_line(&a, &b, color, 1);
+
+	
+	a = ImVec2(left, top);
+	b = ImVec2(left, bottom);
+	imgui_line(&a, &b, color, 1);
+
+	a = ImVec2(right, top);
+	b = ImVec2(right, bottom);
+	imgui_line(&a, &b, color, 1);
+
+}
+
+int depth = 0;
+void dumpWindows(sWindow *window) {
+	sWindow *subwindow;
+	//if(w->Flags & sWF_BEFOREPAINT)
+	//w->OnBeforePaint();
+
+	sFORALL(window->Childs, subwindow)
+	{
+		ImGui::PushID(subwindow);
+		ImGui::Text("Got Window depth=%d: %s %d %d %d %d", depth, subwindow->ToolTip,
+			subwindow->Outer.x0, 
+			subwindow->Outer.y0, 
+			subwindow->Outer.x1, 
+			subwindow->Outer.y1
+		);
+		
 
 
+		
+		imgui_draw_wz_rect(subwindow->Outer, 0xff00ffff);
+		imgui_draw_wz_rect(subwindow->Inner, 0xff0000ff);
+
+
+		
+			if (ImGui::Button("bla")) {
+				subwindow->AddButton(L"bla",sMessage(testMessage,&TestMessage::bla));
+			}
+
+			ImGui::PopID();
+		depth++;
+		dumpWindows(subwindow);
+		depth--;
+	}
+}
 
 void DockScript::imgui() {
-	if (ImGui::Button("bla")) {
-
-	}
-
+	depth = 0;
 	ImGui::Text("Root=%p", sGui->Root);
+	
+
+	dumpWindows(sGui->Root);
 
 }
