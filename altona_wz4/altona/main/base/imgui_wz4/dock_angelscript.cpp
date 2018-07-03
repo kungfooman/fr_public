3953696437d6bf68c7ccfcdf340a0796b4f317ca
kungfooman <lama12345@gmail.com>
2018-07-03 17:29:48 +0200

    add angelscript files and integrate it somewhat

--- altona_wz4/altona/main/base/imgui_wz4/dock_angelscript.cpp
+++ altona_wz4/altona/main/base/imgui_wz4/dock_angelscript.cpp
@@ -8,13 +8,123 @@
 
 DockAngelscript::DockAngelscript() {}
 
+
+#include <angelscript.h>
+#include <scriptstdstring/scriptstdstring.h>
+#include <scriptbuilder/scriptbuilder.h>
+
+class Auto
+{
+public:
+	void starten() {};
+	void bremsen() {};
+};
+
 const char *DockAngelscript::label() {
 	return "Angelscript";
 }
 
+// Implement a simple message callback function
+void MessageCallback(const asSMessageInfo *msg, void *param)
+{
+	const char *type = "ERR ";
+	if (msg->type == asMSGTYPE_WARNING)
+		type = "WARN";
+	else if (msg->type == asMSGTYPE_INFORMATION)
+		type = "INFO";
+	imgui_log("%s (%d, %d) : %s : %s\n", msg->section, msg->row, msg->col, type, msg->message);
+}
+
+#include <string>
+
+//// Print the script string to the standard output stream
+void printz(std::string &msg)
+{
+	imgui_log("%s", msg.c_str());
+}
+
+
 void DockAngelscript::imgui() {
 	ImGui::Text("asd");
 	if (ImGui::Button("test some shit")) {
+
+
+		asIScriptEngine *engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+
+		//engine->RegisterObjectType("Auto", 0, asOBJ_REF);
+		//engine->RegisterObjectMethod("Auto", "void starten()", asMETHOD(Auto, starten), asCALL_THISCALL);
+		//engine->RegisterObjectMethod("Auto", "void bremsen()", asMETHOD(Auto, bremsen), asCALL_THISCALL);
+
+		// Create the script engine
+		//asIScriptEngine *engine = asCreateScriptEngine();
+		// Set the message callback to receive information on errors in human readable form.
+		int r = engine->SetMessageCallback(asFUNCTION(MessageCallback), 0, asCALL_CDECL); assert(r >= 0);
+		// AngelScript doesn't have a built-in string type, as there is no definite standard 
+		// string type for C++ applications. Every developer is free to register its own string type.
+		// The SDK do however provide a standard add-on for registering a string type, so it's not
+		// necessary to implement the registration yourself if you don't want to.
+		RegisterStdString(engine);
+		// Register the function that we want the scripts to call 
+		r = engine->RegisterGlobalFunction("void print(const string &in)", asFUNCTION(printz), asCALL_CDECL); assert(r >= 0);
 		
+
+
+
+
+		// The CScriptBuilder helper is an add-on that loads the file,
+		// performs a pre-processing pass if necessary, and then tells
+		// the engine to build a script module.
+		CScriptBuilder builder;
+		r = builder.StartNewModule(engine, "MyModule");
+		if (r < 0)
+		{
+			// If the code fails here it is usually because there
+			// is no more memory to allocate the module
+			imgui_log("Unrecoverable error while starting a new module.\n");
+			return;
+		}
+		r = builder.AddSectionFromFile("c:\\DATA\\hello.as");
+		if (r < 0)
+		{
+			// The builder wasn't able to load the file. Maybe the file
+			// has been removed, or the wrong name was given, or some
+			// preprocessing commands are incorrectly written.
+			imgui_log("Please correct the errors in the script and try again.\n");
+			return;
+		}
+		r = builder.BuildModule();
+		if (r < 0)
+		{
+			// An error occurred. Instruct the script writer to fix the 
+			// compilation errors that were listed in the output stream.
+			imgui_log("Please correct the errors in the script and try again.\n");
+			return;
+		}
+
+
+
+		// Find the function that is to be called. 
+		asIScriptModule *mod = engine->GetModule("MyModule");
+		asIScriptFunction *func = mod->GetFunctionByDecl("void main()");
+		if (func == 0)
+		{
+			// The function couldn't be found. Instruct the script writer
+			// to include the expected function in the script.
+			imgui_log("The script must have the function 'void main()'. Please add it and try again.\n");
+			return;
+		}
+		// Create our context, prepare it, and then execute
+		asIScriptContext *ctx = engine->CreateContext();
+		ctx->Prepare(func);
+		r = ctx->Execute();
+		if (r != asEXECUTION_FINISHED)
+		{
+			// The execution didn't complete as expected. Determine what happened.
+			if (r == asEXECUTION_EXCEPTION)
+			{
+				// An exception occurred, let the script writer know what happened so it can be corrected.
+				imgui_log("An exception '%s' occurred. Please correct the code and try again.\n", ctx->GetExceptionString());
+			}
+		}
 	}
 }
